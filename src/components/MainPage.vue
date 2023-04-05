dad63fc3e4edbcaaae16f8655965d7daac21db1e
shadowik <zahar.mark@bk.ru>
2023-04-05 19:37:59 +0400

    All 6 Pages

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,6 +4,8 @@
     <div class="item"><SecondPage/></div>
     <div class="item"><ThirdPage/></div>
     <div class="item"><FourthPage/></div>
+    <div class="item"><FifthPage/></div>
+    <div class="item"><SixthPage/></div>
   </vue-scroll-snap>
 </template>
 
@@ -13,8 +15,10 @@ import FirstPage from "@/components/FirstPage.vue";
 import SecondPage from "@/components/SecondPage.vue";
 import ThirdPage from "@/components/ThirdPage.vue";
 import FourthPage from "@/components/FourthPage.vue";
+import FifthPage from "@/components/FifthPage.vue";
+import SixthPage from "@/components/SixthPage.vue";
 export default {
   name: "FullscreenVertical",
-  components: {FourthPage, ThirdPage, SecondPage, FirstPage, VueScrollSnap}
+  components: {SixthPage, FifthPage, FourthPage, ThirdPage, SecondPage, FirstPage, VueScrollSnap}
 };
 </script>
---
+++ src/components/TemplatePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <ComparifyMain
+        :text-first="''"
+        :text-second="''"
+        :image-first="'logo.png'"
+        :image-second="'logo.png'"/>
+</template>
+
+<script>
+import ComparifyMain from "@/components/Comprarify/ComparifyMain.vue";
+
+export default {
+  name: "TemplatePage",
+  components: {ComparifyMain}
+}
+</script>
+
+<style scoped>
+
+</style>
